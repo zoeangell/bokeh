96cb86dd9a8ae2f8101ee1cc8a5cbb79ed55165f
Mateusz Paprocki <mattpap@gmail.com>
2023-02-23 00:04:09 +0100

    Allow to retrieve models with Bokeh.Model.ModelName
    Previously one had to use Bokeh.Model.get("ModelName").

--- bokehjs/src/lib/base.ts
+++ bokehjs/src/lib/base.ts
@@ -1,9 +1,25 @@
 import {isObject, isArray} from "./core/util/types"
 import {values} from "./core/util/object"
+import {isString} from "./core/util/types"
 import {HasProps} from "./core/has_props"
 import {ModelResolver} from "./core/resolvers"
 
 export const default_resolver = new ModelResolver(null)
+
+export const Models = new Proxy(default_resolver, {
+  get(target: ModelResolver, name: string | symbol, _receiver: unknown): unknown {
+    if (name in target) {
+      return (target as any)[name]
+    }
+    if (isString(name)) {
+      const model = target.get(name)
+      if (model != null) {
+        return model
+      }
+    }
+    return undefined
+  },
+})
 
 function is_HasProps(obj: unknown): obj is typeof HasProps {
   return isObject(obj) && (obj as any).prototype instanceof HasProps
